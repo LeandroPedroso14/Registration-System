2b846802580ae3eb6b5a90b77e672c1451656b0c
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-08 19:53:54 -0300

    add print

--- screen.py
+++ screen.py
@@ -4,11 +4,11 @@ class TelaPython:
     def __init__(self):
         #Layout
          layout = [
-             [sg.Txt('Produto',size=(10,0)),sg.Input (size=(38,0))],
-             [sg.Txt('Conteúdo',size=(10,0)),sg.Input (size=(38,0))],
+             [sg.Txt('Produto',size=(10,0)),sg.Input (size=(38,0),key='Produto')],
+             [sg.Txt('Conteúdo',size=(10,0)),sg.Input (size=(38,0),key='Conteúdo')],
              [sg.Txt('Peso'),sg.Input ()],
              [sg.Txt('Qual o tipo do produto ?')],
-             [sg.Checkbox('Liquido'),sg.Checkbox('Comprimido'),sg.Checkbox('Pó')],
+             [sg.Checkbox('Liquido',key='liquido'),sg.Checkbox('Comprimido',key='comprimido'),sg.Checkbox('Pó',key='pó')],
              [sg.Button('Enviar dados')]
          ]
          #Window
@@ -18,7 +18,20 @@ class TelaPython:
          self.button, self.values = window.read()
 
     def Start(self):
-        print(self.values)
+        Produto = self.values['produto']
+        Conteúdo = self.values['conteudo']
+        Peso = self.values['peso']
+        tipo_liquido = self.values['liquído']
+        tipo_comprimido = self.values['comprimido']
+        tipo_pó = self.values['pó']
+        print(f'Prduto:{Produto}')
+        print(f'Conteúdo:{Conteúdo}')
+        print(f'Peso:{Peso}')
+        print(f'tipo_liquido:{tipo_liquido}')
+        print(f'tipo_comprimido:{tipo_comprimido}')
+        print(f'tipo_pó:{tipo_pó}')
+                    
+
 
 tela = TelaPython()
 tela.Start()
