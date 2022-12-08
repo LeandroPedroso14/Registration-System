a60dad699849448acde44988bc1e98b4b21db6f8
Leandro Pedroso <106255930+LeandroPedroso14@users.noreply.github.com>
2022-12-08 16:10:38 -0300

    correcting button character.

--- screen.py
+++ screen.py
@@ -1,18 +1,18 @@
 import PySimpleGUI as sg
 
 class TelaPython:
-    def ___init___(self):
-      #Layout
-      layout = [
-        [sg.Txt('Produto'),sg.Input ()],
-        [sg.Txt('Peso'),sg.Input ()],
-        [sg.Botton('Enviar dados')]
-    ]
-      #Window
-      window = sg.Window("Cadastro do Produto").layout(layout)
+    def __init__(self):
+        #Layout
+         layout = [
+             [sg.Txt('Produto'),sg.Input ()],
+             [sg.Txt('Peso'),sg.Input ()],
+             [sg.Button('Enviar dados')]
+         ]
+         #Window
+         window = sg.Window("Cadastro do Produto").layout(layout)
 
-      #Data extraction
-      self.button, self.values = window.read()
+         #Data extraction
+         self.button, self.values = window.read()
 
     def Start(self):
         print(self.values)
